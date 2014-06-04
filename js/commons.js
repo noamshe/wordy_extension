7bd2cf74a23fa4dfb0875d684546b657811a0c15
noamshe <noamshe@gmail.com>
2014-06-04 23:46:10 +0300

    some changes

--- js/commons.js
+++ js/commons.js
@@ -123,3 +123,18 @@ function addOnclickFunctionalityGlobally() {
 
   head.appendChild(script);
 }
+
+function saveChanges() {
+    // Get a value saved in a form.
+    var theValue = "noam shemesh";
+    // Check that there's some code there.
+    if (!theValue) {
+        message('Error: No value specified');
+        return;
+    }
+    // Save it using the Chrome extension storage API.
+//    chrome.storage.local.set({'value': theValue}, function() {
+        // Notify that we saved.
+        //alert('Settings saved');
+//    });
+}
